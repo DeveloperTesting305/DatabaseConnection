246f7dca7cd692a2dadf7ba8b97d0593728861c2
salmanahmediict <salmanahmediict514@gmail.com>
2020-06-14 16:40:08 +0500

    Initial commit

---
+++ src/DatabaseConnection/DatabaseManager.java
@@ -0,0 +1,18 @@
+package DatabaseConnection;
+
+import javax.swing.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import Remote.DatabaseInterface;
+
+public class DatabaseManager {
+    public static String ip = "127.0.0.1";;
+    public static DatabaseInterface db;
+
+
+    public static void init()throws Exception{
+        Registry reg = LocateRegistry.getRegistry(DatabaseManager.ip, 1099);
+        db = (DatabaseInterface) reg.lookup("rmi://"+DatabaseManager.ip+"/inventoryDatabase");
+        JOptionPane.showMessageDialog(null, "Connection Established");
+    }
+}
